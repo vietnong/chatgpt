a5d3d6c5a6482e1b147afb9edcca63664f1498ee
longnvb01476 <longnvb01476@gmail.com>
2023-04-06 11:20:32 +0700

    change api host

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     const fetchBotResponse = async () => {
         const { data } = await axios.post(
-            "https://chatgpt-ai-app-od21.onrender.com",
+            "https://chatgpt-27b7.onrender.com",
             // "http://localhost:5173",
             { input },
             {
